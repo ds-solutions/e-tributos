552e3a8cceed23f2e6286431477abbfbfd5421d5
Demétrio Santana <softsolutions.apis@gmail.com>
2020-01-03 23:02:11 -0300

    corrigindo os bugs

--- app/src/main/java/com/developer/demetrio/etributos/MainActivity.java
+++ app/src/main/java/com/developer/demetrio/etributos/MainActivity.java
@@ -1,14 +1,43 @@
 package com.developer.demetrio.etributos;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button btEntrar;
+    private EditText login;
+    private EditText senha;
+    long tentativa = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (EditText) findViewById(R.id.idLogin);
+        senha = (EditText) findViewById(R.id.idSenha);
+        btEntrar = (Button) findViewById(R.id.id_button_logar);
+
+        btEntrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onEntrar();
+            }
+        });
+    }
+
+    public void onEntrar() {
+        Toast.makeText(this, "Logando...", Toast.LENGTH_LONG).show();
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Intent carregar = new Intent(getApplicationContext(), Carregar.class);
+        startActivity(carregar);
     }
 }
